777a6ec2b13dffc8adb23ae64d45c61788629a47
yuksel <yonsel.yuksel@gmail.com>
2021-01-23 01:55:22 +0100

    Revert fixed point pkg

--- 1DDR/design/vhdl/MergeOp.vhd
+++ 1DDR/design/vhdl/MergeOp.vhd
@@ -22,13 +22,13 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-library ieee_proposed;
-use ieee_proposed.fixed_pkg.all;
+--library ieee_proposed;
+--use ieee_proposed.fixed_pkg.all;
 
 library work;
 use work.Stream_pkg.all;
 use work.Forecast_pkg.all;
---use work.fixed_generic_pkg_mod.all;
+use work.fixed_generic_pkg_mod.all;
 
 
 entity MergeOp is
