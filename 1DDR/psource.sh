951efa78e059a0580259797e280ca8742b54696d
yuksel <yonsel.yuksel@gmail.com>
2021-03-04 14:38:33 +0100

    Update with new input type the psource

--- 1DDR/psource.sh
+++ 1DDR/psource.sh
@@ -2,4 +2,4 @@
 
 export CL_DIR=$(pwd)
 export FLETCHER_AWS_SIM_BUILD_DIR=~/thesis_journals/resources/fletcher-aws/examples/tpch6/1DDR/software/runtime
-export TEST_RECORDBATCH_BASE=$DATASET_DIR/2021_01_13_12_26/l_recordbatch
+export TEST_RECORDBATCH_BASE=$DATASET_DIR/2021_03_04_01_01/l_recordbatch
