6065ac60badad2c599983b567ba725b437fdc10b
yuksel <yonsel.yuksel@gmail.com>
2021-03-27 15:28:54 +0100

    Update gitignore

--- 1DDR/psource.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export CL_DIR=$(pwd)
-export FLETCHER_AWS_SIM_BUILD_DIR=~/thesis_journals/resources/fletcher-aws/examples/tpch6/1DDR/software/runtime
-export TEST_RECORDBATCH_BASE=$DATASET_DIR/10000rowslineitemrb/l_recordbatch
