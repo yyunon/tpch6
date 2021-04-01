62e917f592dffcec26d1810a817cb7994116c10b
yuksel <yonsel.yuksel@gmail.com>
2021-04-02 00:34:47 +0200

    Refactor flopoco converter:
    - Make axi stream piped.
    - Try to make it work with subnormal numbers, still not working.

--- 1DDR/design/vhdl/Float_to_Fixed.vhd
+++ 1DDR/design/vhdl/Float_to_Fixed.vhd
@@ -4,13 +4,19 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.std_logic_misc.ALL;
 USE ieee.numeric_std.ALL;
 
+LIBRARY ieee_proposed;
+USE ieee_proposed.fixed_pkg.ALL;
+
 LIBRARY work;
 USE work.Stream_pkg.ALL;
 USE work.ParallelPatterns_pkg.ALL;
 USE work.Forecast_pkg.ALL;
+--use work.fixed_generic_pkg_mod.all;
 ENTITY Float_to_Fixed IS
   GENERIC (
 
+    FIXED_LEFT_INDEX : INTEGER;
+    FIXED_RIGHT_INDEX : INTEGER;
     DATA_WIDTH : NATURAL;
     INPUT_MIN_DEPTH : NATURAL;
     OUTPUT_MIN_DEPTH : NATURAL;
@@ -38,7 +44,7 @@ END Float_to_Fixed;
 
 ARCHITECTURE Behavioral OF Float_to_Fixed IS
   -- Floating  point IP used by xilinx
-  COMPONENT floating_point_0
+  COMPONENT floating_point_0_2
     PORT (
       aclk : IN STD_LOGIC;
       s_axis_a_tvalid : IN STD_LOGIC;
@@ -61,19 +67,20 @@ ARCHITECTURE Behavioral OF Float_to_Fixed IS
       X : IN STD_LOGIC_VECTOR(63 DOWNTO 0);
       R : OUT STD_LOGIC_VECTOR(11 + 52 + 2 DOWNTO 0));
   END COMPONENT;
-  COMPONENT FP2Fix_11_52M_18_45_S_T_F400_uid2 IS
+  COMPONENT FP2Fix_11_52M_32_31_S_NT_F400_uid2 IS
     PORT (
-      clk : IN STD_LOGIC;
+      clk, rst : IN STD_LOGIC;
       I : IN STD_LOGIC_VECTOR(11 + 52 + 2 DOWNTO 0);
       O : OUT STD_LOGIC_VECTOR(63 DOWNTO 0));
   END COMPONENT;
   --
-
   SIGNAL conv_data_valid : STD_LOGIC := '0';
   SIGNAL conv_data_ready : STD_LOGIC := '0';
   SIGNAL conv_data_dvalid : STD_LOGIC := '0';
   SIGNAL conv_data_last : STD_LOGIC := '0';
   SIGNAL conv_data : STD_LOGIC_VECTOR(63 DOWNTO 0) := (OTHERS => '0');
+  SIGNAL result_data : STD_LOGIC_VECTOR(63 DOWNTO 0) := (OTHERS => '0'); --MUX to the output of converter.
+  SIGNAL float_type : STD_LOGIC_VECTOR(1 DOWNTO 0);--MUX to the output of converter.
 
   SIGNAL ops_valid : STD_LOGIC;
   SIGNAL ops_dvalid : STD_LOGIC;
@@ -81,7 +88,15 @@ ARCHITECTURE Behavioral OF Float_to_Fixed IS
   SIGNAL ops_last : STD_LOGIC;
   SIGNAL ops_data : STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0);
 
-  SIGNAL flopoco_data : STD_LOGIC_VECTOR(DATA_WIDTH + 1 DOWNTO 0);
+  -- Pipeline signals
+  SIGNAL data, data_d1, data_d2, data_d3, data_d4, data_d5, data_d6, data_d7 : STD_LOGIC_VECTOR(11 + 52 + 2 DOWNTO 0);
+  SIGNAL ready, ready_d1, ready_d2, ready_d3, ready_d4, ready_d5, ready_d6, ready_d7 : STD_LOGIC;
+  SIGNAL last, last_d1, last_d2, last_d3, last_d4, last_d5, last_d6, last_d7 : STD_LOGIC;
+  SIGNAL dvalid, dvalid_d1, dvalid_d2, dvalid_d3, dvalid_d4, dvalid_d5, dvalid_d6, dvalid_d7 : STD_LOGIC;
+  SIGNAL valid, valid_d1, valid_d2, valid_d3, valid_d4, valid_d5, valid_d6, valid_d7 : STD_LOGIC;
+
+  CONSTANT ZERO : STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0) := (OTHERS => '0');
+
 BEGIN
 
   op_in_sync : StreamBuffer
@@ -106,57 +121,6 @@ BEGIN
     out_data(63 DOWNTO 0) => ops_data
   );
 
-  none_converter :
-  IF CONVERTER_TYPE = "none" GENERATE
-    conv_data <= ops_data;
-    conv_data_last <= ops_last;
-    conv_data_dvalid <= ops_dvalid;
-    conv_data_valid <= ops_valid;
-    ops_ready <= conv_data_ready;
-  END GENERATE;
-
-  xilinx_converter :
-  IF CONVERTER_TYPE = "xilinx_ip" GENERATE
-    data_converter : floating_point_0
-    PORT MAP(
-      aclk => clk,
-      s_axis_a_tvalid => ops_valid,
-      s_axis_a_tready => ops_ready,
-      s_axis_a_tdata => ops_data,
-      s_axis_a_tuser(0) => ops_dvalid,
-      s_axis_a_tlast => ops_last,
-      m_axis_result_tvalid => conv_data_valid,
-      m_axis_result_tready => conv_data_ready,
-      m_axis_result_tdata => conv_data,
-      m_axis_result_tuser(0) => conv_data_dvalid,
-      m_axis_result_tlast => conv_data_last
-    );
-  END GENERATE;
-
-  flopoco_converter :
-  IF CONVERTER_TYPE = "flopoco_big_numbers" GENERATE
-    -- Flopoco has its own type, which is simply IEEE.
-    -- Yet, it uses flags for normalization etc. 
-    -- First, convert it to flopoco then to fixed point in this 
-    -- part. 
-    -- NT => Non truncated.
-    -- [45,-18]
-    ieee_to_flopoco : InputIEEE_11_52_to_11_52
-    PORT MAP(
-      clk => clk,
-      rst => reset,
-      X => ops_data,
-      R => flopoco_data
-    );
-
-    flopoco_to_fixed : FP2Fix_11_52M_18_45_S_T_F400_uid2
-    PORT MAP(
-      clk => clk,
-      I => flopoco_data,
-      O => conv_data
-    );
-  END GENERATE;
-
   out_buf : StreamBuffer
   GENERIC MAP(
     DATA_WIDTH => DATA_WIDTH + 2,
@@ -177,4 +141,116 @@ BEGIN
     out_data(64) => out_dvalid,
     out_data(63 DOWNTO 0) => out_data
   );
+
+  none_converter :
+  IF CONVERTER_TYPE = "none" GENERATE
+    conv_data <= ops_data;
+    conv_data_last <= ops_last;
+    conv_data_dvalid <= ops_dvalid;
+    conv_data_valid <= ops_valid;
+    ops_ready <= conv_data_ready;
+  END GENERATE;
+
+  xilinx_converter :
+  IF CONVERTER_TYPE = "xilinx" GENERATE
+    data_converter : floating_point_0_2
+    PORT MAP(
+      aclk => clk,
+      s_axis_a_tvalid => ops_valid,
+      s_axis_a_tready => ops_ready,
+      s_axis_a_tdata => ops_data,
+      s_axis_a_tuser(0) => ops_dvalid,
+      s_axis_a_tlast => ops_last,
+      m_axis_result_tvalid => conv_data_valid,
+      m_axis_result_tready => conv_data_ready,
+      m_axis_result_tdata => conv_data,
+      m_axis_result_tuser(0) => conv_data_dvalid,
+      m_axis_result_tlast => conv_data_last
+    );
+  END GENERATE;
+
+  flopoco_converter_big :
+  IF CONVERTER_TYPE = "flopoco" GENERATE
+    reg_process :
+    PROCESS (clk)
+    BEGIN
+      IF clk'event AND clk = '1' THEN
+
+        ready_d1 <= ready_d2;
+        ready_d2 <= ready_d3;
+        ready_d3 <= ready_d4;
+        ready_d4 <= ready_d5;
+        ready_d5 <= ready_d6;
+        ready_d6 <= ready_d7;
+        ready_d7 <= ready;
+
+        valid_d1 <= valid;
+        valid_d2 <= valid_d1;
+        valid_d3 <= valid_d2;
+        valid_d4 <= valid_d3;
+        valid_d5 <= valid_d4;
+        valid_d6 <= valid_d5;
+        valid_d7 <= valid_d6;
+
+        last_d1 <= last;
+        last_d2 <= last_d1;
+        last_d3 <= last_d2;
+        last_d4 <= last_d3;
+        last_d5 <= last_d4;
+        last_d6 <= last_d5;
+        last_d7 <= last_d6;
+
+        dvalid_d1 <= dvalid;
+        dvalid_d2 <= dvalid_d1;
+        dvalid_d3 <= dvalid_d2;
+        dvalid_d4 <= dvalid_d3;
+        dvalid_d5 <= dvalid_d4;
+        dvalid_d6 <= dvalid_d5;
+        dvalid_d7 <= dvalid_d6;
+
+        data_d1 <= data;
+        data_d2 <= data_d1;
+        data_d3 <= data_d2;
+        data_d4 <= data_d3;
+        data_d5 <= data_d4;
+        data_d6 <= data_d5;
+        data_d7 <= data_d6;
+      END IF;
+    END PROCESS;
+    -- Flopoco has its own type, which is simply IEEE.
+    -- Yet, it uses flags for normalization etc. 
+    -- First, convert it to flopoco then to fixed point in this 
+    -- part. We spend 7 clk cycles: 1 clk cycle for ieee to flopoco and the
+    -- rest is for flopoco to fixed point converter.
+    -- NT => Non truncated.
+    -- [45,-18]
+    ops_ready <= ready_d1;
+    last <= ops_last;
+    dvalid <= ops_dvalid;
+    valid <= ops_valid;
+    ieee_to_flopoco : InputIEEE_11_52_to_11_52
+    PORT MAP(
+      clk => clk,
+      rst => reset,
+      X => ops_data,
+      R => data
+    );
+    flopoco_to_fixed : FP2Fix_11_52M_32_31_S_NT_F400_uid2
+    PORT MAP(
+      clk => clk,
+      rst => reset,
+      I => data_d1,
+      O => result_data
+    );
+    conv_data_dvalid <= dvalid_d7;
+    conv_data_valid <= valid_d7;
+    conv_data_last <= last_d7;
+    --float_type <= data_d7(65 DOWNTO 64);
+    --conv_data <= ZERO WHEN (float_type = (1 DOWNTO 0 => '0')) ELSE
+    --result_data(DATA_WIDTH - 1 DOWNTO DATA_WIDTH - 1 - FIXED_LEFT_INDEX) & "0000" & result_data(DATA_WIDTH - 2 - FIXED_LEFT_INDEX DOWNTO 4);
+    --conv_data <= ZERO WHEN (float_type = (1 DOWNTO 0 => '0')) ELSE
+    --result_data;
+    conv_data <= result_data;
+    ready <= conv_data_ready OR (NOT valid_d7);
+  END GENERATE;
 END Behavioral;
